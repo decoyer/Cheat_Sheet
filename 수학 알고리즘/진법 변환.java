0bf201d6fb289bd5922341039fb7cd487ef612ec
decoyer <87348583+decoyer@users.noreply.github.com>
2024-11-13 15:08:25 +0900

    Create 진법 변환.java

---
+++ 수학 알고리즘/진법 변환.java
@@ -0,0 +1,13 @@
+private static void formation() {
+    // 10진수 → 2진수
+    Integer.toBinaryString(1);
+
+    // 10진수 → 8진수
+    Integer.toOctalString(1);
+
+    // 10진수 → 16진수
+    Integer.toHexString(1);
+
+    // s → n진수
+    Integer.parseInt("1", 10);
+}
