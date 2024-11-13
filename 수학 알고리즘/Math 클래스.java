4abcca7056e636aa9f54a65df74d7dc1c24b270b
decoyer <87348583+decoyer@users.noreply.github.com>
2024-11-13 15:03:56 +0900

    Create Math 클래스.java

---
+++ 수학 알고리즘/Math 클래스.java
@@ -0,0 +1,28 @@
+private static void math() {
+    // 최대값(2)
+    Math.max(1, 2);
+
+    // 최소값(1)
+    Math.min(1, 2);
+
+    // 2의 제곱근(1.414...)
+    Math.sqrt(2);
+
+    // 2의 2제곱(4)
+    Math.pow(2, 2);
+
+    // 올림(4)
+    Math.ceil(3.14);
+
+    // 내림(3)
+    Math.floor(3.14);
+
+    // 반올림(3)
+    Math.round(3.14);
+
+    // 절댓값(3)
+    Math.abs(3.14);
+
+    // 0부터 1까지 랜덤 값 반환
+    Math.random();
+}
