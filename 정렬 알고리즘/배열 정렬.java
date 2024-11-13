521f3f91c89049fa1be970091c2b78970e49eea4
decoyer <87348583+decoyer@users.noreply.github.com>
2024-11-13 13:54:42 +0900

    Update 배열 정렬.java

--- 정렬 알고리즘/배열 정렬.java
+++ 정렬 알고리즘/배열 정렬.java
@@ -3,12 +3,20 @@ public static void sort() {
 
     // 오름차순 정렬
     Arrays.sort(arr);
-  
+
+    Integer[] tmp = new Integer[arr.length];
+
+    for (int i = 0; i < tmp.length; i++)
+        tmp[i] = arr[i];
+
+    // 내림차순 정렬
+    Arrays.sort(tmp, Collections.reverseOrder());
+
+    // 내림차순 정렬(ArrayList 활용)
     ArrayList<Integer> list = new ArrayList<>();
 
     for (int i : arr)
         list.add(i);
 
-    // 내림차순 정렬
     Collections.sort(list, Collections.reverseOrder());
 }
